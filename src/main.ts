fecd84104366baff37d645ede695db5d368c1c3d
张浩杰 <zhanghaojie@ai-wsd.com>
2023-08-22 15:06:04 +0800

    feat: 配置基本脚手架功能

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { createSSRApp } from "vue";
+import "uno.css";
+import pinia from "./store";
 import App from "./App.vue";
 export function createApp() {
   const app = createSSRApp(App);
+  app.use(pinia);
   return {
     app,
   };
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,15 +1,34 @@
 <template>
   <view class="content">
     <image class="logo" src="/static/logo.png" />
-    <view class="text-area">
-      <text class="title">{{ title }}</text>
+    <view>
+      <text>{{ title }}</text>
     </view>
+    <view m-5>
+      <uni-number-box v-model="numberStore.number" @change="changeValue" />
+    </view>
+    <button @click="numberStore.resetNumber" size="mini" plain>重置数字</button>
+    <button m-5 @click="getData" size="mini" plain>网络请求</button>
   </view>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-const title = ref('Hello')
+import { ref } from "vue";
+import { useNumberStore } from "@/store";
+import { http } from "@/utils/http";
+const numberStore = useNumberStore();
+const title = ref("Hello uniapp");
+const changeValue = (value: number) => {
+  console.log("返回数值：", value);
+  numberStore.setNumber(value);
+};
+const getData = async () => {
+  const res = http({
+    method: "GET",
+    url: "/test/hello",
+  });
+  console.log(res);
+};
 </script>
 
 <style>
@@ -27,11 +46,6 @@ const title = ref('Hello')
   margin-left: auto;
   margin-right: auto;
   margin-bottom: 50rpx;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
 }
 
 .title {
